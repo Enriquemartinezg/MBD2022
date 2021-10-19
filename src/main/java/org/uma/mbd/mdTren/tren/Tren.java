1a3038b60b583f97fe61bac7b2cdfb6be4913031
enrique <enriquemartinezgar@gmail.com>
2021-10-19 18:06:05 +0200

    Commit in laboratory class

---
+++ src/main/java/org/uma/mbd/mdTren/tren/Tren.java
@@ -0,0 +1,4 @@
+package org.uma.mbd.mdTren.tren;
+
+public class Tren {
+}
---
+++ src/main/java/org/uma/mbd/mdTren/tren/Vagon.java
@@ -0,0 +1,13 @@
+package org.uma.mbd.mdTren.tren;
+
+public class Vagon {
+    private int capacidad;
+    private int carga;
+
+    public Vagon(int capacidad){
+        this.capacidad = capacidad;
+        carga = 0;
+    }
+
+
+}
